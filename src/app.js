90a5bc19e2101fe986a95235b6626a0ca9687393
Nelreina <nelreina@gmail.com>
2018-03-04 16:55:51 -0400

    feature/initialize: Inititalize Express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.info('Redis Course');
+const express = require('express');
+const path = require('path');
+const redis = require('redis');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+// Body Parser initi
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Method Override (to be able to send delete request)
+app.use(methodOverride('_method'));
+
+app.listen(PORT, () => {
+  console.info(`App is running on port ${PORT}`);
+});
