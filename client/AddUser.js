3bb85cbfb61c1df06f6809d8b040c10ea8dc3261
Nelreina <nelreina@gmail.com>
2018-03-04 23:21:01 -0400

    feature/add-user: added post fetch in api and handled form

--- client/AddUser.js
+++ client/AddUser.js
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { isEmpty } from 'lodash';
+import { post } from './api';
 
-const AddUser = () => {
-  return (
-    <div>
-      <h1>Add User</h1>
-    </div>
-  );
+const isValidForm = form => {
+  if (isEmpty(form)) return false;
+  if ('id' in form) {
+    const keys = Object.keys(form);
+    const fields = [];
+    keys.forEach(k => {
+      if (k !== 'id') {
+        fields.push(k);
+      }
+    });
+    if (fields.length === 0) return false;
+    return true;
+  } else {
+    return false;
+  }
 };
+const initState = { form: {}, validForm: false };
+class AddUser extends Component {
+  state = initState;
+  handleInput = evt => {
+    const { name, value } = evt.target;
+    const { form } = this.state;
+    form[name] = value;
+    const validForm = isValidForm(form);
+    this.setState({ form, validForm });
+  };
+  addUser = async evt => {
+    evt.preventDefault();
+    if (!this.state.validForm) {
+      return;
+    }
+    evt.target.reset();
+    const data = await post('/add-user', this.state.form);
+    console.info(data);
+    this.setState(initState);
+  };
+  render() {
+    return (
+      <form onSubmit={this.addUser}>
+        <div className="form-group">
+          <label htmlFor="id">Redis ID</label>
+          <input
+            type="text"
+            name="id"
+            className="form-control"
+            placeholder="Enter ID"
+            onChange={this.handleInput}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            className="form-control"
+            placeholder="Enter user First Name"
+            onChange={this.handleInput}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            name="lastName"
+            className="form-control"
+            placeholder="Enter user Last Name"
+            onChange={this.handleInput}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            name="age"
+            className="form-control"
+            placeholder="Enter user Age"
+            onChange={this.handleInput}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Add
+        </button>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      </form>
+    );
+  }
+}
 
 export default AddUser;
--- client/api.js
+++ client/api.js
@@ -1,14 +1,35 @@
 const API = 'http://localhost:9000/api';
 const name = 'API Fetch Error';
+
+const respError = resp => ({
+  name,
+  message: `${resp.status} - ${resp.statusText}`
+});
+
 export const get = async path => {
   const resp = await fetch(`${API}${path}`);
   console.info(resp);
   if (resp.ok) {
     return resp.json();
   } else {
-    throw {
-      name,
-      message: `${resp.status} - ${resp.statusText}`
-    };
+    throw respError(resp);
+  }
+};
+
+export const post = async (path, data) => {
+  const postOptions = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  console.info(postOptions);
+  const resp = await fetch(`${API}${path}`, postOptions);
+  console.info(resp);
+  if (resp.ok) {
+    return resp.json();
+  } else {
+    throw respError(resp);
   }
 };
