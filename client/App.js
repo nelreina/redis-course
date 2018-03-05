b3d6fe1c20797df55d313c1b880fbd0d933adda3
Nelreina <nelreina@gmail.com>
2018-03-04 20:50:16 -0400

    feature/cleanup: removed test code

--- client/App.js
+++ client/App.js
@@ -5,21 +5,11 @@ import Home from './Home';
 import AddUser from './AddUser';
 import { get } from './api';
 class App extends Component {
-  state = {};
   setStateAsync = state => {
     return new Promise(resolve => {
       this.setState(state, resolve);
     });
   };
-  async componentDidMount() {
-    try {
-      const data = await get('/ping');
-      console.info(data);
-      await this.setStateAsync({ data });
-    } catch (error) {
-      await this.setStateAsync({ error });
-    }
-  }
 
   render() {
     return (
@@ -29,7 +19,6 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/add-user" component={AddUser} />
         </Switch>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
